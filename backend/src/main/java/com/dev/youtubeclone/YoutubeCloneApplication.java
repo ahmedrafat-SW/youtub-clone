a9d46deaf02872011c8acb8d053f137cb4acdbba
Ahmed Rafat <ahmed.rafat.abdelbar@gmail.com>
2023-08-30 23:16:51 +0300

    Initalizing the spring boot and angular modules.
    we have add two maven modules to our project
    the first module for back-end which is
    the working directory for spring boot api
    the second module for fornt-end which is
    the working directory for the angular
    source code.

---
+++ backend/src/main/java/com/dev/youtubeclone/YoutubeCloneApplication.java
@@ -0,0 +1,12 @@
+package com.dev.youtubeclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeCloneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeCloneApplication.class, args);
+    }
+
+}
